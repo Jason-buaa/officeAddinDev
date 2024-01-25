039943ef94c7a352844908db442f3bccaba37db3
Jason_buaa <waterworth@qq.com>
2024-01-25 19:09:07 +0800

    增加筛选和排序表格

---
+++ 筛选和排序表格/taskpane/taskpane.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    // Assign event handlers and other initialization logic.
+    document.getElementById("create-table").onclick = () => tryCatch(createTable);
+    document.getElementById("filter-table").onclick = () => tryCatch(filterTable);
+    document.getElementById("sort-table").onclick = () => tryCatch(sortTable);
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+
+  }
+});
+async function createTable() {
+  await Excel.run(async (context) => {
+
+      // Queue table creation logic here.
+      const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+      const expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+      expensesTable.name = "ExpensesTable";
+      // Queue commands to populate the table with data.
+      expensesTable.getHeaderRowRange().values =
+      [["Date", "Merchant", "Category", "Amount"]];
+  
+      expensesTable.rows.add(null /*add at the end*/, [
+          ["1/1/2017", "The Phone Company", "Communications", "120"],
+          ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+          ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+          ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+          ["1/11/2017", "Bellows College", "Education", "350.1"],
+          ["1/15/2017", "Trey Research", "Other", "135"],
+          ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+      ]);
+      // TODO3: Queue commands to format the table.
+      expensesTable.columns.getItemAt(3).getRange().numberFormat = [['\u20AC#,##0.00']];
+      expensesTable.getRange().format.autofitColumns();
+      expensesTable.getRange().format.autofitRows();
+      await context.sync();
+  });
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+  try {
+      await callback();
+  } catch (error) {
+      // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+      console.error(error);
+  }
+}
+
+async function filterTable() {
+  await Excel.run(async (context) => {
+
+      // Queue commands to filter out all expense categories except
+      //        Groceries and Education.
+      const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+      const expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+      const categoryFilter = expensesTable.columns.getItem('Category').filter;
+      categoryFilter.applyValuesFilter(['Education', 'Groceries']);
+      await context.sync();
+  });
+}
+
+async function sortTable() {
+  await Excel.run(async (context) => {
+
+      // Queue commands to sort the table by Merchant name.
+      const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+      const expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+      const sortFields = [
+          {
+              key: 1,            // Merchant column。SortField 对象的 key 属性是用于排序的列的从零开始编制索引。 表中的行按照所引用列中的值进行排序。
+              ascending: false,
+          }
+      ];
+      
+      expensesTable.sort.apply(sortFields);
+      await context.sync();
+  });
+}
