98ae6d0540e51494395f94b5716223dd404dcce9
Jason_buaa <waterworth@qq.com>
2024-01-27 16:41:46 +0800

    增加打开对话框示例。

---
+++ 打开对话框/src/dialogs/popup.js
@@ -0,0 +1,21 @@
+Office.onReady((info) => {
+    //  Assign handler to the OK button.
+    document.getElementById("ok-button").onclick = () => tryCatch(sendStringToParentPage);
+});
+
+//  Create the OK button handler.
+
+function sendStringToParentPage() {
+    const userName = document.getElementById("name-box").value;
+    Office.context.ui.messageParent(userName);
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+    try {
+        await callback();
+    } catch (error) {
+        // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+        console.error(error);
+    }
+}
